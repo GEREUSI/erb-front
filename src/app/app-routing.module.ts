58d2695ad3040fd6cedafb2fc903084e49520fa0
Aurimas Šakalys <aurimas.sakalys@stud.vgtu.lt>
2021-02-03 22:07:59 +0200

    Base layout and styling for sign up page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './body';
+import { SignupComponent } from './body/signup/signup.component';
+import { ROUTES } from './constants/routes.const'
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: ROUTES.SignUp, component: SignupComponent},
+    {path: ROUTES.Home, component: HomeComponent},
+    {path: '', redirectTo: ROUTES.HomeRedirect, pathMatch: 'full'},
+    {path: '**', redirectTo: ROUTES.HomeRedirect},
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './body';
+import { SignupComponent } from './body/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
